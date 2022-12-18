095178454d28e267ed83adb962816223a293caa8
SerhyRymar <108816720+SerhyRymar@users.noreply.github.com>
2022-12-18 12:06:22 +0000

    Deploying to gh-pages from @ SerhyRymar/goit-js-hw-11@03b3cde7129a162ce1eaa21de63f22c32797da64 🚀

--- index.2797bfb8.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form"),document.querySelector("input"),document.querySelector(".gallery"),document.querySelector(".more-container__button");
-//# sourceMappingURL=index.2797bfb8.js.map
